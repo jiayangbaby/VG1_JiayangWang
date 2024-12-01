c2bfb6564cef2ff3531d2c8d07fbff29d8244990
Jiayang Wang <143467227+jiayangbaby@users.noreply.github.com>
2024-11-30 21:53:38 -0600

    Quest 10 Done!

--- Assets/Code/Adventure/PlayerController.cs
+++ Assets/Code/Adventure/PlayerController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Adventure
 {
+    public enum Direction {
+        Up=0,
+        Down=1,
+        Left=2,
+        Right=3
+    }
+
     public class PlayerController : MonoBehaviour
     {
         //Outlets
         Rigidbody2D _rigidbody;
         Animator _animator;
+        SpriteRenderer _spriteRenderer;
+        public Transform[] attackZones;
 
         //Configuration
         public KeyCode keyUp;
@@ -15,12 +24,17 @@ namespace Adventure
         public KeyCode keyLeft;
         public KeyCode keyRight;
         public float moveSpeed;
+        public Sprite[] sprites;
+
+        //State Tracking
+        public Direction facingDirection;
 
         // Methods
         void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>(); 
 
         }
 
@@ -29,15 +43,36 @@ namespace Adventure
         {
             float movementSpeed = _rigidbody.velocity.sqrMagnitude;
             _animator.SetFloat("speed", movementSpeed);
-            if (movementSpeed > 0.1f) {
+            if (movementSpeed > 0.1f)
+            {
                 _animator.SetFloat("movementX", _rigidbody.velocity.x);
                 _animator.SetFloat("movementY", _rigidbody.velocity.y);
             }
 
-            if (Input.GetKeyDown(KeyCode.Space)) {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
                 _animator.SetTrigger("attack");
-            }
 
+
+                //Convert the enumeration to an index
+                int facingDirectionIndex = (int)facingDirection;
+
+                //Get an attack zone from index
+                Transform attackZone = attackZones[facingDirectionIndex];
+
+                //What objects are within a circle at that attack zone?
+                Collider2D[] hits = Physics2D.OverlapCircleAll(attackZone.position, 0.1f);
+
+                //Handle each hit target
+                foreach (Collider2D hit in hits)
+                {
+                    Breakable breakableObject = hit.GetComponent<Breakable>();
+                    if (breakableObject)
+                    {
+                        breakableObject.Break();
+                    }
+                }
+            }
         }
 
         void FixedUpdate()
@@ -59,6 +94,17 @@ namespace Adventure
                 _rigidbody.AddForce(Vector2.right * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
             }
 
+        }
+
+        void LateUpdate()
+        {
+            for (int i = 0; i < sprites.Length; i++) {
+                if (_spriteRenderer.sprite == sprites[i]) {
+                    facingDirection = (Direction)i;
+                    break;
+                }
+            }
+            
         }
 
 
